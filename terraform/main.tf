a38a3311b422611e4da15b970a834e270ad47a63
Quaid DeLacluyse <qdelacluyse@stephensgroup.com>
2025-01-30 21:22:30 -0500

    Added the other pieces

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,7 @@ resource "azurerm_container_app" "container_app" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   container_app_environment_id = azurerm_container_app_environment.container_env.id
+  revision_mode = "Single"
 
   template {
     container {
